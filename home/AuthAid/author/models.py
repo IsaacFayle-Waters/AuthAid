620f5c77206f994c6a97b3d3cf9273d8d8bf3834
IsaacFayle-Waters <zakfayl@gmail.com>
2021-01-17 13:31:47 +0000

    Search function now not defined in every class, found way to make a general usage function. Added some dummy/test entries to db.

--- home/AuthAid/author/models.py
+++ home/AuthAid/author/models.py
@@ -3,6 +3,15 @@ from django.db import models
 name_length = 500
 description_length = 2000
 notes_all = models.TextField(default='')
+
+"""Search for assignments of an instance of a class in instances of other classes.
+#EXAMPLE OF USAGE:
+    #w = World.objects.get(pk=1)
+    #search_ass(NarrativeGeneralInfo,world=w.id)
+"""
+def search_ass(Target, **kwargs):
+    return Target.objects.filter(**kwargs)
+
 #Basically a project. Allows several 'novels'(ngi's) set in one world/universe.
 class World(models.Model):
     name = models.CharField(max_length=name_length)
@@ -14,10 +23,13 @@ class World(models.Model):
 
     """def search_ngi(self):
         ngi = NarrativeGeneralInfo.objects.filter(world=self.id)
-        return ngi"""
+        return ngi
     #Search for references to self in instances of other classes, and return list of those instances
     def search_general(self,Target):
-            return Target.objects.filter(world=self.id)
+        return Target.objects.filter(world=self.id)
+    #Works
+    def sergen(self,Target,**kwargs):
+        return Target.objects.filter(**kwargs)"""
 
 #An individual Narrative, i.e. a single novel, within the World.
 class NarrativeGeneralInfo(models.Model):
@@ -32,10 +44,6 @@ class NarrativeGeneralInfo(models.Model):
     def __str__(self):
         return self.title
 
-        # Search for references to self in instances of other classes, and return list of those instances
-    def search_general(self,Target):
-        return Target.objects.filter(ngi=self.id)
-
 class Chapter(models.Model):
     title = models.CharField(max_length=name_length,default='')
     description = models.CharField(max_length=description_length,default='')
@@ -47,10 +55,6 @@ class Chapter(models.Model):
     def __str__(self):
         return self.title
 
-        # Search for references to self in instances of other classes, and return list of those instances
-    def search_general(self, Target):
-        return Target.objects.filter(chapter=self.id)
-
 class Scene(models.Model):
     description = models.CharField(max_length=description_length, default='')
     notes = notes_all
@@ -60,13 +64,8 @@ class Scene(models.Model):
     ngi = models.ForeignKey(NarrativeGeneralInfo, on_delete=models.CASCADE, default='')
     world = models.ForeignKey(World, on_delete=models.CASCADE, default='')
 
-
     def __str__(self):
         return self.description
-
-    #NOT TESTED. Not sure if workable with many to many.
-    def search_general(self, Target):
-        return Target.objects.filter(scene=self.id)
 
 class Character(models.Model):
     name = models.CharField(max_length=name_length, default='')
