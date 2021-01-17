47fc59f0d3f8995184fbabc831abd9f3ead3d05a
IsaacFayle-Waters <zakfayl@gmail.com>
2021-01-17 02:20:36 +0000

    Added search_general function to all non-leaf models. Removed migrations, and re-migrated db.

--- home/AuthAid/author/models.py
+++ home/AuthAid/author/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-#Basically a project. Allows several 'novels' set in one world.
-
 name_length = 500
 description_length = 2000
 notes_all = models.TextField(default='')
-
+#Basically a project. Allows several 'novels'(ngi's) set in one world/universe.
 class World(models.Model):
     name = models.CharField(max_length=name_length)
     description = models.CharField(max_length=description_length)
@@ -17,11 +15,9 @@ class World(models.Model):
     """def search_ngi(self):
         ngi = NarrativeGeneralInfo.objects.filter(world=self.id)
         return ngi"""
-
+    #Search for references to self in instances of other classes, and return list of those instances
     def search_general(self,Target):
-
-        search = Target.objects.filter(world=self.id)
-        return search
+            return Target.objects.filter(world=self.id)
 
 #An individual Narrative, i.e. a single novel, within the World.
 class NarrativeGeneralInfo(models.Model):
@@ -29,45 +25,62 @@ class NarrativeGeneralInfo(models.Model):
     description = models.CharField(max_length=description_length,default='')
     notes = notes_all
 
-    setting = models.CharField(max_length=2000)
-    genre = models.CharField(max_length=200)
-    world = models.ForeignKey(World,on_delete=models.CASCADE)
+    setting = models.CharField(max_length=2000, default='')
+    genre = models.CharField(max_length=200, default='')
+    world = models.ForeignKey(World,on_delete=models.CASCADE, default='')
 
     def __str__(self):
         return self.title
+
+        # Search for references to self in instances of other classes, and return list of those instances
+    def search_general(self,Target):
+        return Target.objects.filter(ngi=self.id)
 
 class Chapter(models.Model):
     title = models.CharField(max_length=name_length,default='')
     description = models.CharField(max_length=description_length,default='')
     notes = notes_all
     ngi = models.ForeignKey(NarrativeGeneralInfo,on_delete=models.CASCADE)
-
+    #TODO: DECIDE HOW TO HANDLE CHAPTER NUMBERS
     chapter_number = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
 
+        # Search for references to self in instances of other classes, and return list of those instances
+    def search_general(self, Target):
+        return Target.objects.filter(chapter=self.id)
+
 class Scene(models.Model):
     description = models.CharField(max_length=description_length, default='')
     notes = notes_all
-    chapter = models.ForeignKey(Chapter,on_delete=models.CASCADE)
-    ngi = models.ForeignKey(NarrativeGeneralInfo, on_delete=models.CASCADE)
+    time_and_or_date = models.CharField(max_length=100, default='')
 
-    time_and_or_date = models.CharField(max_length=100)
+    chapter = models.ForeignKey(Chapter,on_delete=models.CASCADE, default='')
+    ngi = models.ForeignKey(NarrativeGeneralInfo, on_delete=models.CASCADE, default='')
+    world = models.ForeignKey(World, on_delete=models.CASCADE, default='')
+
 
     def __str__(self):
         return self.description
+
+    #NOT TESTED. Not sure if workable with many to many.
+    def search_general(self, Target):
+        return Target.objects.filter(scene=self.id)
 
 class Character(models.Model):
     name = models.CharField(max_length=name_length, default='')
     description = models.CharField(max_length=description_length, default='')
     notes = notes_all
 
+    world = models.ForeignKey(World, on_delete=models.CASCADE, null=True)
     scenes = models.ManyToManyField(Scene)
-    age = models.IntegerField(default=0)
-    gender = models.CharField(max_length=30)
-    motive = models.CharField(max_length=50)
+    chapters = models.ManyToManyField(Chapter)
 
+    age = models.IntegerField(default=0)
+    gender = models.CharField(max_length=30, default='')
+    motive = models.CharField(max_length=50, default='')
+    #Basic relationships between characters, in this case whether they know each other
     acquaintances = models.ManyToManyField('self',related_name='persons_known')
 
     def __str__(self):
@@ -77,6 +90,8 @@ class Location(models.Model):
     name = models.CharField(max_length=name_length, default='')
     description = models.CharField(max_length=description_length, default='')
     notes = notes_all
+
+    world = models.ForeignKey(World, on_delete=models.CASCADE, default='')
     scenes = models.ManyToManyField(Scene)
     chapters = models.ManyToManyField(Chapter)
 
