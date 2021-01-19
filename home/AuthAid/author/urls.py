6a5005f3bd3a0b2ba1001aabdd7870aef17a5efd
IsaacFayle-Waters <zakfayl@gmail.com>
2021-01-19 15:45:05 +0000

    Added blank templates for all classes, fixed their basic path inplace.

--- home/AuthAid/author/urls.py
+++ home/AuthAid/author/urls.py
@@ -4,7 +4,13 @@ from . import views
 
 app_name = 'author'
 
-urlpatterns = [path('',views.IndexView.as_view(),name='index'),
+urlpatterns = [
+    path('',views.IndexView.as_view(), name='index'),
+    path('<int:pk>/',views.WorldView.as_view(), name='world'),
+    path('',views.NgiView.as_view(), name='ngi'),
+    path('',views.ChapterView.as_view(), name='chapter'),
+    path('',views.SceneView.as_view(), name='scene'),
+    path('',views.CharacterView.as_view(), name='character'),
+    path('',views.LocationView.as_view(),name='location')
 
-
-               ]
+]
--- home/AuthAid/author/views.py
+++ home/AuthAid/author/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
-from .models import World
+from .models import World, NarrativeGeneralInfo, Chapter, Scene, Character, Location, search_ass
 # Create your views here.
 
 class IndexView(generic.ListView):
@@ -9,3 +9,21 @@ class IndexView(generic.ListView):
     context_object_name = 'world_list'
     def get_queryset(self):
         return World.objects.order_by('name')
+
+class WorldView(generic.ListView):
+    pass
+
+class NgiView(generic.ListView):
+    pass
+
+class ChapterView(generic.ListView):
+    pass
+
+class SceneView(generic.ListView):
+    pass
+
+class CharacterView(generic.ListView):
+    pass
+
+class LocationView(generic.ListView):
+    pass
